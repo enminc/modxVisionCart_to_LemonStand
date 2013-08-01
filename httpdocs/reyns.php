f74b186c50521d311f225296c7b4268c3eb89d9f
jtreen <justin@thelim3y.com>
2013-08-01 09:13:39 -0700

    option matrix code added

--- httpdocs/reyns.php
+++ httpdocs/reyns.php
@@ -1,15 +1,16 @@
 <?php
+	ini_set('memory_limit', '-1');
+	set_time_limit(0);
 
 	$conn = mysqli_connect('localhost', 'reyns', 'reyns', 'reyns_modx');
 	if(mysqli_connect_errno()) {
 		die('Could not connect: ' . mysqli_connect_error());
 	}
 
-
 	$sel = "select * from modx_visioncart_products where active=1 and parent=0 order by id asc";
 	$res = mysqli_query($conn, $sel);
 
-	echo "Found {$res->num_rows} products";
+	echo "Found {$res->num_rows} products<br /><br />";
 
 	$products = array();
 
@@ -45,7 +46,16 @@
 			
 			$cats[] =$cat;
 		}
-		$v['categories'] = $cats;
+
+		if(count($cats)) {
+			$catstr = $cats[0]['name'];
+			
+			for($i = 1, $j = count($cats); $i < $j; $i++):
+				$catstr .= "|{$cats[0]['name']}=>{$cats[$i]['name']}";
+			endfor;
+		}
+
+		$v['categories'] = $catstr;
 		mysqli_free_result($res);
 
 
@@ -77,11 +87,147 @@
 
 			$opts[] = $opt;
 		}
+
+		$optstr = '';
+		if(count($opts)) {
+			foreach($opts as $opt):
+				$optstr .= ' ' . $opts[0]['name'] . ': ';
+			
+				foreach($opt['vals'] as $val):
+					$optstr .= "{$val}|";
+				endforeach;
+			endforeach;
+		}
+		$optstr = ltrim($optstr, ' ');
+		$optstr = rtrim($optstr, '|');
+		$v['lemon_options'] = $optstr;
 		$v['options'] = $opts;
+		
+		$v['option_matrix'] = calcOptionMatrix($v['orig_id'], $v['options']);
+
 		mysqli_free_result($res);
+	}
+	
+	
+	// Insert temp products
+	mysqli_query($conn, "TRUNCATE TABLE lemon_products_temp");
+	
+	$ins = "INSERT INTO lemon_products_temp (orig_id, name, url_name, long_description, title, sku, price, total_units_in_stock) VALUES ";
+	$ins2 = "INSERT INTO lemon_options_temp (orig_id, name, value) VALUES ";
+
+	foreach($products as $p):
+		$ins .= "({$p['orig_id']},";
+		$ins .= '"' . mysqli_real_escape_string($conn, $p['name']) . '",';
+		$ins .= '"' . mysqli_real_escape_string($conn, $p['url_name']) . '",';
+		$ins .= '"' . mysqli_real_escape_string($conn, $p['long_description']) . '",';
+		$ins .= '"' . mysqli_real_escape_string($conn, $p['title']) . '",';
+		$ins .= '"' . mysqli_real_escape_string($conn, $p['sku']) . '",';
+		$ins .= "{$p['price']}, {$p['total_units_in_stock']}),";
+		
+		foreach($p['options'] as $opt):
+			foreach($opt['vals'] as $oval):
+				$ins2 .= "({$p['orig_id']},";
+				$ins2 .= '"' . mysqli_real_escape_string($conn, $opt['name']) . '",';
+				$ins2 .= '"' . mysqli_real_escape_string($conn, $oval) . '"),';
+/* 				echo $ins2 . '<br />'; */
+			endforeach;
+		endforeach;
+	endforeach;
+
+	$ins = rtrim($ins, ',') . ';';
+	$ins2 = rtrim($ins2, ',') . ';';
+ 
+	if(mysqli_query($conn, $ins) === FALSE) {
+		echo('error: ' . mysqli_error($conn) . '<br /><br /><br />');
+		echo $ins . '<br />';
+	}
+
+	if(mysqli_query($conn, $ins2) === FALSE) {
+		echo $ins2 . '<br />';
+		echo('error: ' . mysqli_error($conn) . '<br /><br /><br />');
 	}
 
 
 	print '<pre>';print_r($products);print '</pre>';
 	
 	mysqli_close($conn);
+
+
+
+/*********************************************************************************************************************/
+/*********************************************************************************************************************/
+/*********************************************************************************************************************/
+
+
+function calcOptionMatrix($pid, $options) {
+	global $conn;
+	$mat = array();
+
+	if(count($options) < 2) {
+		return $mat;
+	}
+
+	mysqli_query($conn, "TRUNCATE TABLE lemon_optmatrix");
+
+	$ins = "INSERT INTO lemon_optmatrix (opt_id, name, value) VALUES ";
+
+	foreach($options as $opt):
+		foreach($opt['vals'] as $val):
+			$ins .= "({$opt['id']},";
+			$ins .= '"' . mysqli_real_escape_string($conn, $opt['name']) . '",';
+			$ins .= '"' . mysqli_real_escape_string($conn, $val) . '"),';
+		endforeach;
+
+/* 		print '<pre>';print_r($val);print '</pre>'; */
+/* 		print '<pre>';print_r($ins);print '</pre>'; */
+	endforeach;
+
+	$ins = rtrim($ins, ',') . ';';
+
+/* 	print '<pre>';print_r($ins);print '</pre>'; */
+
+	$res = mysqli_query($conn, $ins);
+	if($res === FALSE) {
+		echo $ins . '<br />';
+		echo('error: ' . mysqli_error($conn) . '<br /><br /><br />');
+	}
+
+
+	// Calc values we need
+	$tvals = array();
+	$tables = array();
+	$where = array();
+
+	for($i = 0; $i < count($options); $i++) {
+		
+		$tvals[] = "t{$i}.opt_id, t{$i}.name, t{$i}.value";
+		$tables[] = "lemon_optmatrix t{$i}";
+		$where[] = "t{$i}.opt_id={$options[$i]['id']}";
+	}
+	$tvals = implode(',', $tvals);
+	$tables = implode(',', $tables);
+	$where = implode(' AND ', $where);
+
+/*
+	print '<pre>';print_r($tvals);print '</pre>';
+	print '<pre>';print_r($tables);print '</pre>';
+	print '<pre>';print_r($where);print '</pre>';
+*/
+	
+	$sel = "SELECT {$tvals} from lemon_optmatrix CROSS JOIN {$tables} WHERE {$where}";
+/* 	print '<pre>';print_r($sel);print '</pre>'; */
+
+	$res = mysqli_query($conn, $sel);
+	if($res === FALSE) {
+		echo $sel . '<br />';
+		echo('error: ' . mysqli_error($conn) . '<br /><br /><br />');
+	}
+
+	while($row = mysqli_fetch_array($res)) {
+		$mat[] = $row;
+	}
+
+/* 	print '<pre>';print_r($matrix);print '</pre>'; */
+
+	return $mat;
+}
